ab5a4242189e6f53723ed06fafddb960702fd596
Sindt <czindt@gmail.com>
2017-12-20 20:13:44 +0100

    added bash scripts for docker

--- install.sh
+++ install.sh
@@ -1,16 +1,4 @@
-Vagrant.configure("2") do |config|
-
-  config.vm.box = "ubuntu/xenial64"
-  config.vm.network "forwarded_port", guest: 80, host: 3000
-  config.vm.synced_folder "../", "/synced_folder"
-
-  config.vm.provider "virtualbox" do |vb|
-    vb.name = "si_blockchain"
-    vb.memory = "4096"
-    vb.cpus = "2"
-  end
-
-  config.vm.provision "shell", privileged: false, inline: <<-SHELL
+#!/bin/sh
 	sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -27,5 +15,5 @@ Vagrant.configure("2") do |config|
 	sudo apt-get install -y docker-ce
 	sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
-  SHELL
-end
+
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+	sudo docker-compose up -d --scale node1-service=4
